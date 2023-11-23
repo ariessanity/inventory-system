873be139a9684b91dc45fa3709402b1595f5bfb3
ariessanity <ianorillo21@gmail.com>
2023-11-23 20:44:05 +0800

    fix build

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,10 +8,10 @@ async function bootstrap() {
   app.enableCors();
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "..", "client", "build")));
-
+    app.use(express.static(path.resolve(__dirname, '../dist')));
     app.use('*', (req, res) => {
-      res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+      res.end();
     });
   }
 
