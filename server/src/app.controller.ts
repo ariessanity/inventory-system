ad4f205ac334c3bfef2ba38d2ed3ddcec3057460
ariessanity <ianorillo21@gmail.com>
2023-11-24 11:09:42 +0800

    fix build

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get, Render, Req, Res } from '@nestjs/common';
 import { Response } from 'express';
-import path from 'path';
 
 @Controller()
 export class AppController {
   @Get()
   get(@Res() res: Response) {
     res.sendFile('index.html', {
-      root: '../client/build/server/pages',
+      root: '../../client/build/server/pages',
     });
   }
 }
