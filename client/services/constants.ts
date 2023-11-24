34ec7b33222bcfd18098e92888795d953132e3ef
ariessanity <ianorillo21@gmail.com>
2023-11-25 07:55:53 +0800

    fix date format

--- client/services/constants.ts
+++ client/services/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'whttps://inventory-system-server-roan.vercel.app/'
+export const BASE_URL = 'https://inventory-system-server-roan.vercel.app/'
 
 // export const BASE_URL = 'http://localhost:4000/'
