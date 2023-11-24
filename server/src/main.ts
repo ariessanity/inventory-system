d49b87bc0a50880378459a3da47b93ed030b5416
ariessanity <ianorillo21@gmail.com>
2023-11-24 08:35:20 +0800

    fix build

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path, { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, '../dist')));
-    app.use('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../dist/index.html'));
-      res.end();
-    });
-  }
-
+  
   await app.listen(4000);
 }
 bootstrap();
