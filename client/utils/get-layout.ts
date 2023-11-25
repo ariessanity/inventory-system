9066dbd05488786bcc7c2e612015641d35e9cbc2
ariessanity <ianorillo21@gmail.com>
2023-11-25 13:11:32 +0800

    fix build

--- client/utils/get-layout.ts
+++ client/utils/get-layout.ts
@@ -3,7 +3,7 @@ import { AuthenticatedLayout, UnauthenticatedLayout } from '@/components/Layout'
 import { Fragment } from 'react'
 
 export function getLayout(pathname: string) {
-  if (pathname.match(/^\/login|signup|forgot-password/)) {
+  if (pathname.match(/^\/login|signup|forgot-password|/)) {
     return UnauthenticatedLayout
   }
 
