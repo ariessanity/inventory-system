61c6730f770fc3e70ab8e954d6042113532b1ed5
ariessanity <ianorillo21@gmail.com>
2023-11-25 09:45:08 +0800

    fix date

--- client/utils/date-format.ts
+++ client/utils/date-format.ts
@@ -1,8 +1,15 @@
-import { formatInTimeZone } from "date-fns-tz";
+import { addHours, format, parseISO } from "date-fns";
+import { formatInTimeZone, zonedTimeToUtc } from "date-fns-tz";
 
-export const formatDate = (isoDate: Date) => {
-  // const formattedDate = format(new Date(localTime), "MMM dd, yyyy h:mma");
-  const formattedDate = formatInTimeZone(new Date(isoDate), 'Asia/Manila', 'MMM dd, yyyy h:mma')
+export const formatDate = (isoDate: string) => {
+  const dateInGMTPlus8 = parseISO(isoDate);
+  const dateInUTC = addHours(dateInGMTPlus8, -8);
+
+  const formattedDate = formatInTimeZone(
+    dateInUTC,
+    "Asia/Manila",
+    "MMM dd, yyyy h:mma"
+  );
 
   return formattedDate;
 };
